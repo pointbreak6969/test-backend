9b20544b57ce1bfdc9d99b9450cbc1ea73f0bb89
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-04-22 14:49:54 +0545

    additional configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import connectDB from "./db/db.js";
 import 'dotenv/config'
+const PORT = process.env.PORT || 5000;
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+}).catch((e)=>{
+    console.log("MongoDB connection error", e);
+});
