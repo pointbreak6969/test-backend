a63edfb4a83cb2e60ff94ff16ba3584221dfc037
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-04-26 15:39:50 +0545

    Refactor user controller to include watch history retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
@@ -378,6 +379,53 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   }
   return res.status(200).json(new ApiResponse(200, channel[0], "user channel fetched succesfully"));
 });
+
+
+const getWatchHistroy = (asyncHandler (async (req, res)=>{
+const user = await User.aggregate([
+  {
+    $match: {
+      _id: new mongoose.Types.ObjectId(req.user._id)
+    }
+  },
+  {
+    $lookup: {
+      from : "videos",
+      localField: "watchHistory",
+      foreignField: "_id",
+      as: "watchHistroy",
+      pipeline: [
+        {
+          $lookup: {
+            from : "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "owner",
+            pipeline: [
+              {
+                $project: {
+                  fullname: 1,
+                  username: 1,
+                  avatar: 1
+                }
+              }, {
+                $addFields: {
+                  owner: {
+                    $first: "$owner"
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+])
+return res.status(200).json(
+  new ApiResponse(200, user[0].getWatchHistroy, "Watch Histroy fetched successfully")
+)
+}))
 export {
   registerUser,
   loginUser,
@@ -389,4 +437,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistroy
 };
