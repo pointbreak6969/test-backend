4ab0eb65b894cb4baf943499d1b6cf51af0c2a2e
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-04-30 21:35:21 +0545

    fixed bugs of vidoe uploading

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,6 +3,11 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { APIError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Comment } from "../models/comment.model.js";
+import { Playlist } from "../models/playlist.model.js";
+// import {Like} from "../models/like.model.js"
 
 const uploadVideo = asyncHandler(async (req, res) => {
   try {
@@ -16,7 +21,8 @@ const uploadVideo = asyncHandler(async (req, res) => {
     const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
     const videoFile = await uploadOnCloudinary(videoLocalPath);
     const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
-
+    console.log(req.user)
+    console.log(req.user?._id);
     const video = Video.create({
       title,
       description,
@@ -25,20 +31,50 @@ const uploadVideo = asyncHandler(async (req, res) => {
       owner: req.user?._id,
       duration: videoFile?.duration,
     });
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(201, {
+    return res.status(200).json(
+      new ApiResponse(
+        201,
+        {
           ...video._doc,
           videoFile: videoFile.url,
           thumbnail: thumbnail.url,
-        }, "Video Published Successfully")
-      );
+        },
+        "Video Published Successfully"
+      )
+    );
   } catch (error) {
     console.error("Error while publishing video :: ", error);
-      throw new APIError(500, error?.message || 'Server Error while uploading video');
+    throw new APIError(
+      500,
+      error?.message || "Server Error while uploading video"
+    );
   }
 });
 
-
+const getAllVideos = asyncHandler(async (req, res) => {
+  const {
+    page = 1,
+    limit = 110,
+    query = "",
+    sortBy = "createdAt",
+    sortType = 1,
+    userId,
+  } = req.query;
+  const matchCondition = {
+    $or: [
+      {
+        title: { $regex: query, $options: "i" },
+      },
+      {
+        description: {
+          $regex: query,
+          $options: "i",
+        },
+      },
+    ],
+  };
+  if (userId) {
+    matchCondition.owner = new mongoose.Types.ObjectId(userId);
+  }
+});
 export { uploadVideo };
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { uploadVideo } from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
+router.use(verifyJWT)
 router.route("/uploadVideo").post(
   upload.fields([
     { name: "video", maxCount: 1 },
